074ed5227623cbc9e9546ce2d2bd9830a1fbe864
bhargav <sbbhargav1993@gmail.com>
2022-06-15 07:48:45 +0530

    repeated code placed into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+	def back_link(path)
+		link_to 'back', path, class: 'btn btn-primary'
+	end
+	def delete_link(path,style_class)
+		button_to 'delete', path, method: :delete, class: style_class
+	end
+
+	def show_link(path,style_class)
+		link_to 'show', path, class: style_class
+	end
+
+	def edit_link(path,style_class)
+		link_to 'edit', path, class: style_class
+	end
+
 end
