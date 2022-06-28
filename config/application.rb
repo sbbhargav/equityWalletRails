afcc4ab3532512afa7a9a6c88c56bf47b9b86e55
bhargav <sbbhargav1993@gmail.com>
2022-06-28 07:58:48 +0530

    modified loader to 7

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module RailsProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 7.0
+    config.load_defaults 7.0
 
-    config.autoload = :classic
+    # config.autoload = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
